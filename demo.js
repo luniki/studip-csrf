c6d8d865bd39714722fc3bc7f14e2a544e3fc64d
luniki <github@luniki.de>
2019-05-01 12:57:02 +0200

    Update demo.js

--- demo.js
+++ demo.js
@@ -17,3 +17,23 @@ fetch(
         body: JSON.stringify({"content":"blubbi","context_type":"public"})
     }
 )
+
+fetch(
+    "https://studip-test.uos.de/plugins.php/argonautsplugin/blubber-postings/e6017903d6cb0e212613a614e5939d92",
+    {
+        method: "GET",
+        credentials: "include"
+    }
+).then(function(response) { console.log("fetch posting", response) }).catch(function (error) { console.log("error", error) })
+
+fetch(
+    "https://studip-test.uos.de/plugins.php/argonautsplugin/blubber-postings/e6017903d6cb0e212613a614e5939d92/comments",
+    {
+        method: "POST",
+        credentials: "include",
+        headers: new Headers({
+            "Content-Type": "application/vnd.api+json"
+        }),
+        body: JSON.stringify({ data: { type: "blubber-postings", attributes: { content: "a comment blubb" } } })
+    }
+).then(function(response) { console.log("post comment", response) }).catch(function (error) { console.log("error", error) })
