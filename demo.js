bea695e6277f8b275eafba47f330e09f640a89d7
luniki <github@luniki.de>
2019-05-01 12:42:07 +0200

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,1 +1,11 @@
-console.log(17)
+await fetch(
+    "https://studip-test.uos.de/api.php/blubber/postings",
+    {
+        method: "POST",
+        credentials: "include",
+        headers: new Headers({
+            "Content-Type": "application/json;charset=utf-8"
+        }),
+        body: JSON.stringify({"content":"blubbi","context_type":"public"})
+    }
+)
